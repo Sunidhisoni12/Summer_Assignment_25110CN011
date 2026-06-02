315ca6edbc51e41334466d0d67d66485c6aee2c5
Sunidhi Soni <sunidhisoni31@gmail.com>
2026-06-02 12:22:49 +0530

    day-2 assignment is uploaded

---
+++ day-2/ques1.java
@@ -0,0 +1,29 @@
+// Que.1 Write a program to Find sum of digits of a number.
+
+import java.util.*;
+
+public class ques1 {
+
+    public static int Sum(int a) {
+
+        int sum = 0;
+        while (a > 0) {
+            int lastDigit = a % 10;
+            sum += lastDigit;
+            a /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the  number : ");
+        int a = sc.nextInt();
+        int result = Sum(a);
+        System.out.println("Sum of the digits is: " + result);
+    }
+
+}
+
+
