431004a90c9518ed49c826c1e14118c05ee7d0f4
Sunidhi Soni <sunidhisoni31@gmail.com>
2026-06-08 14:47:52 +0530

    day-9 assignment is uploaded

---
+++ day-9/ques34.java
@@ -0,0 +1,18 @@
+//ques34. write a program to print reverse number triangle
+import java.util.*;
+
+public class ques34 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for(int i = n; i >= 1; i--) {
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
