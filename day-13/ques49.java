29b449e1b06747c8cf35c1cac562f6a2e1d9c7fe
Sunidhi Soni <sunidhisoni31@gmail.com>
2026-06-13 00:21:12 +0530

    day-13 assignment is uploaded

---
+++ day-13/ques49.java
@@ -0,0 +1,26 @@
+//ques49. write a program to input and display array
+import java.util.*;
+
+public class ques49 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
---
+++ day-13/ques52.java
@@ -0,0 +1,35 @@
+//ques52.write a program to count even and odd elements
+import java.util.*;
+
+public class ques52 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Number of even elements = " + evenCount);
+        System.out.println("Number of odd elements = " + oddCount);
+
+        sc.close();
+    }
+}
