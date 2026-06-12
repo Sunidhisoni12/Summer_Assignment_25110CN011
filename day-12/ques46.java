755f912b340c59ab08fd00d8ebf1020b875b1aff
Sunidhi Soni <sunidhisoni31@gmail.com>
2026-06-12 16:16:15 +0530

    day-12 assignment is uploaded

---
+++ day-12/ques46.java
@@ -0,0 +1,33 @@
+//ques46. write a program to write function for armstrong
+import java.util.*;
+
+public class ques46 {
+
+    public static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum = sum + (digit * digit * digit);
+            n = n / 10;
+        }
+
+        return original == sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (isArmstrong(n)) {
+            System.out.println("Armstrong Number");
+        } else {
+            System.out.println("Not an Armstrong Number");
+        }
+
+        sc.close();
+    }
+}
