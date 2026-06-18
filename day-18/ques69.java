be16f8744f02dc6fe5a5c6ad8374374335ed67d0
Sunidhi Soni <sunidhisoni31@gmail.com>
2026-06-18 14:01:49 +0530

    day-18 assignment was uploaded

---
+++ day-18/ques69.java
@@ -0,0 +1,31 @@
+//ques69.write a problem to bubble sort
+import java.util.*;
+
+public class ques69 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
---
+++ day-18/ques71.java
@@ -0,0 +1,42 @@
+//ques71. write  a program to binary search
+import java.util.*;
+
+public class ques71 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int key = sc.nextInt();
+
+        int low = 0;
+        int high = n - 1;
+        int found = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                found = mid;
+                break;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        if (found != -1) {
+            System.out.println("Element found at index " + found);
+        } else {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
